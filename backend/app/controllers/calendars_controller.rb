3e59632f826fc523d402d38112c7f63cfc258fc1
rumusa <19166472+rumusa@users.noreply.github.com>
2022-06-06 20:36:03 +0200

    Refactor #3 calendar#create, new calendar#verify_create method

--- backend/app/controllers/calendars_controller.rb
+++ backend/app/controllers/calendars_controller.rb
@@ -19,9 +19,7 @@ class CalendarsController < ApplicationController
 
   # POST /calendars
   def create
-    calendar_params_obj = calendar_params
-    calendar_status = calendar_params_obj[:active]
-    set_calendar_status if active?(calendar_status)
+    calendar_params_obj = verify_create(calendar_params)
     @calendar = current_user.calendars.create(calendar_params_obj)
     if @calendar.save
       render json: @calendar, status: :created, location: @calendar
@@ -32,21 +30,9 @@ class CalendarsController < ApplicationController
 
   # PATCH/PUT /calendars/1
   def update
-    # calendar_status = calendar_params[:active]
-    # calendar_name = calendar_params[:name]
+    # calendar_params_obj = verify_update(calendar_params)
 
-    # if active?(calendar_status)
-
-    # if
-    #   active_calendar = current_user.calendars.find_by active: 'true'
-    #   active_calendar.active = 'false'
-    # else
-    #   active_calendar = current_user.calendars.find_by active: 'false'
-    #   active_calendar.active = 'true'
-    # end
-    # active_calendar.save
-
-    if @calendar.update(calendar_params)
+    if @calendar.update(calendar_params_obj)
       render json: @calendar
     else
       render json: @calendar.errors, status: :unprocessable_entity
@@ -60,6 +46,37 @@ class CalendarsController < ApplicationController
 
   private
 
+  # Verify calendar create
+  def verify_create(calendar_params_obj)
+    if active?(calendar_params_obj[:active])
+      unless first_calendar?
+        active_calendar = current_user.calendars.find_by active: 'true'
+        active_calendar.active = 'false'
+        active_calendar.save
+      end
+    elsif first_calendar?
+      calendar_params_obj[:active] = 'true'
+    end
+    calendar_params_obj
+  end
+
+  # Verify calendar update
+  # def verify_update(calendar_params_obj)
+
+  # end
+
+  # Check if calendar first
+  def first_calendar?
+    all_calendars = current_user.calendars.all
+    'true' if all_calendars.size.zero?
+  end
+
+  # Check if calendar last
+  def last_calendar?
+    all_calendars = current_user.calendars.all
+    'true' if all_calendars.size == 1
+  end
+
   # Use callbacks to share common setup or constraints between actions.
   def set_calendar
     @calendar = Calendar.find(params[:id])
@@ -68,13 +85,6 @@ class CalendarsController < ApplicationController
   # Only allow a list of trusted parameters through.
   def calendar_params
     params.require(:calendar).permit(:name, :active)
-  end
-
-  # Set active calendar status to false
-  def set_calendar_status
-    active_calendar = current_user.calendars.find_by active: 'true'
-    active_calendar.active = 'false'
-    active_calendar.save
   end
 
   # Verify if value is true
